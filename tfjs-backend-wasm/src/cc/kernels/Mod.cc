f7b20f4c50b4634f521fc82591b13f7d1123b60a
kinsei0916 <kinsei0916@gmail.com>
2021-08-20 03:29:42 +0900

    minor change

--- tfjs-backend-wasm/src/cc/kernels/Mod.cc
+++ tfjs-backend-wasm/src/cc/kernels/Mod.cc
@@ -22,6 +22,25 @@
 #include "tfjs-backend-wasm/src/cc/binary.h"
 #include "tfjs-backend-wasm/src/cc/util.h"
 
+namespace {
+
+template <typename T>
+inline T mod(T a, T b) {
+  struct FloatMod {
+    float operator()(const float lhs, const float rhs) const {
+      return std::fmod(lhs, rhs);
+    }
+  };
+  using ModFunc = typename std::conditional<std::is_integral<T>::value,
+                                            std::modulus<T>, FloatMod>::type;
+  ModFunc mod_func;
+  T trunc_mod = mod_func(a, b);
+  return (trunc_mod != 0) && ((b < 0) != (trunc_mod < 0)) ? (trunc_mod + b)
+                                                          : trunc_mod;
+}
+
+}  // namespace
+
 namespace tfjs {
 namespace wasm {
 // We use C-style API to interface with Javascript.
@@ -35,22 +54,10 @@ void Mod(const int a_id, const size_t* a_shape_ptr, const int a_shape_len,
          const DType dtype, const int out_id) {
   switch (dtype) {
     case DType::float32:
-      binary_f32(a_id, b_id, out_id, [](float a, float b) {
-        float rem = fmodf(a, b);
-        if ((a < 0 && b < 0) || (a >= 0 && b >= 0))
-          return rem;
-        else
-          return fmodf(rem + b, b);
-      });
+      binary_f32(a_id, b_id, out_id, mod);
       break;
     case DType::int32:
-      binary_i32(a_id, b_id, out_id, [](int a, int b) {
-        int rem = a % b;
-        if ((a < 0 && b < 0) || (a >= 0 && b >= 0))
-          return rem;
-        else
-          return (rem + b) % b;
-      });
+      binary_i32(a_id, b_id, out_id, mod);
       break;
     default:
       util::warn("Mod for tensor ids %d and %d failed. Unsupported dtype %d",
